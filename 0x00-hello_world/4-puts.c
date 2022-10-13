ece3f91b32d60d03d538209fc092f39e2b7b0692
Eyoel20 <eyoelahadu20@gmail.com>
2022-10-13 19:09:36 +0000

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,8 @@
  *hello puts
  */
 
-int main (void)
+int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
