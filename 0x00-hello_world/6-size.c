e8c8d335214f885009f6ef33725e707da4f15ab2
Eyoel20 <eyoelahadu20@gmail.com>
2022-10-13 19:37:25 +0000

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+/**
+ *main - executes printf
+ *
+ *retrun: returns 0
+ */
 int main(void)
 {
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
@@ -8,4 +12,4 @@ int main(void)
 	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %d byte (s)\n", sizeof(float));
 	return (0);
-} 
+}
